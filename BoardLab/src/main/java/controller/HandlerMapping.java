4c569a6889356a89e48194ceb041a2a061f34e4e
Kim BoHyeong <kerong12@naver.com>
2023-06-01 14:35:11 +0900

    HandlerMapping

--- BoardLab/src/main/java/controller/HandlerMapping.java
+++ BoardLab/src/main/java/controller/HandlerMapping.java
@@ -3,6 +3,11 @@ package controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import controller.board.DeleteBoardController;
+import controller.board.GetBoardController;
+import controller.board.GetBoardListController;
+import controller.board.InsertBoardController;
+import controller.board.UpdateBoardController;
 import controller.user.InsertUserController;
 import controller.user.LoginController;
 import controller.user.LogoutController;
@@ -14,6 +19,12 @@ public class HandlerMapping {
 		mappings.put("/insertUser.do", new InsertUserController());
 		mappings.put("/login.do", new LoginController());
 		mappings.put("/logout.do", new LogoutController());
+		mappings.put("/insertBoard.do", new InsertBoardController());
+		mappings.put("/getBoardList.do", new GetBoardListController());
+		
+		mappings.put("/updateBoard.do", new UpdateBoardController());
+		mappings.put("/deleteBoard.do", new DeleteBoardController());
+		mappings.put("/getBoard.do", new GetBoardController());
 	}
 	public Controller getController(String path) {
 		return mappings.get(path);   // hashmap 을 가지고 있어서 리턴하면 위에 로그인이나 로그아웃이 반환됨
